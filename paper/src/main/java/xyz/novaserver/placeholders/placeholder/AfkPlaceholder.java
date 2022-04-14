bfe71369c93b7d29e05a02c34e142a153e85da97
Lui798 <42649201+Lui798@users.noreply.github.com>
2022-04-14 03:13:43 -0500

    Use reflection for non-softdepend plugins

--- paper/src/main/java/xyz/novaserver/placeholders/placeholder/AfkPlaceholder.java
+++ paper/src/main/java/xyz/novaserver/placeholders/placeholder/AfkPlaceholder.java
@@ -1,6 +1,5 @@
 package xyz.novaserver.placeholders.placeholder;
 
-import com.earth2me.essentials.Essentials;
 import ninja.leaping.configurate.ConfigurationNode;
 import org.bukkit.Bukkit;
 import xyz.novaserver.placeholders.common.Placeholders;
@@ -8,18 +7,31 @@ import xyz.novaserver.placeholders.common.data.PlayerData;
 import xyz.novaserver.placeholders.placeholder.type.Placeholder;
 import xyz.novaserver.placeholders.placeholder.type.RelationalType;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.UUID;
+
 public class AfkPlaceholder extends Placeholder implements RelationalType {
-    private final Essentials essentials;
+    private final Object essentials;
 
     public AfkPlaceholder(Placeholders plugin) {
         super(plugin, "afk");
-        this.essentials = (Essentials) Bukkit.getPluginManager().getPlugin("Essentials");
+        this.essentials = Bukkit.getPluginManager().getPlugin("Essentials");
+    }
+
+    private boolean isAfk(UUID uuid) {
+        try {
+            Object user = essentials.getClass().getMethod("getUser", UUID.class).invoke(essentials, uuid);
+            return (boolean) user.getClass().getMethod("isAfk").invoke(user);
+        } catch (InvocationTargetException | IllegalAccessException | NoSuchMethodException e) {
+            // Essentials not loaded
+            return false;
+        }
     }
 
     @Override
     public String get(PlayerData viewer, PlayerData player) {
         final ConfigurationNode node = getPlaceholders().getConfig();
-        if (!essentials.getUser(player.getUuid()).isAfk()) {
+        if (!isAfk(player.getUuid())) {
             return "";
         } else {
             return viewer.isResourcePackApplied() ? node.getNode("status", "afk", "rp").getString()
--- paper/src/main/java/xyz/novaserver/placeholders/placeholder/VoicePlaceholder.java
+++ paper/src/main/java/xyz/novaserver/placeholders/placeholder/VoicePlaceholder.java
@@ -3,29 +3,49 @@ package xyz.novaserver.placeholders.placeholder;
 import ninja.leaping.configurate.ConfigurationNode;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.RegisteredServiceProvider;
-import su.plo.voice.PlasmoVoiceAPI;
 import xyz.novaserver.placeholders.common.Placeholders;
 import xyz.novaserver.placeholders.common.data.PlayerData;
 import xyz.novaserver.placeholders.placeholder.type.Placeholder;
 import xyz.novaserver.placeholders.placeholder.type.RelationalType;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.UUID;
+
 public class VoicePlaceholder extends Placeholder implements RelationalType {
-    private PlasmoVoiceAPI plasmoApi;
+    private Object plasmoApi;
+    private Method hasVoiceChat;
 
-    public VoicePlaceholder(Placeholders plugin) {
-        super(plugin, "voice", 2000);
+    public VoicePlaceholder(Placeholders placeholders) {
+        super(placeholders, "voice", 2000);
 
-        RegisteredServiceProvider<PlasmoVoiceAPI> provider = Bukkit.getServicesManager().getRegistration(PlasmoVoiceAPI.class);
-        if (provider != null) {
-            plasmoApi = provider.getProvider();
+        if (Bukkit.getPluginManager().isPluginEnabled("PlasmoVoice")) {
+            try {
+                Class<?> classPlasmoApi = Class.forName("su.plo.voice.PlasmoVoiceAPI");
+                RegisteredServiceProvider<?> provider = Bukkit.getServicesManager().getRegistration(classPlasmoApi);
+                if (provider != null) {
+                    plasmoApi = provider.getProvider();
+                    hasVoiceChat = classPlasmoApi.getMethod("hasVoiceChat", UUID.class);
+                }
+            } catch (ClassNotFoundException | NoSuchMethodException | NullPointerException ignored) {
+                // PlasmoVoice not loaded
+            }
+        }
+    }
+
+    private boolean hasVoiceChat(UUID uuid) {
+        try {
+            return plasmoApi != null && (boolean) hasVoiceChat.invoke(plasmoApi, uuid);
+        } catch (IllegalAccessException | InvocationTargetException ignored) {
+            // PlasmoVoice not loaded
+            return false;
         }
     }
 
     @Override
     public String get(PlayerData viewer, PlayerData player) {
         final ConfigurationNode node = getPlaceholders().getConfig();
-        final boolean hasVoice = plasmoApi != null && plasmoApi.hasVoiceChat(player.getUuid());
-
+        final boolean hasVoice = hasVoiceChat(player.getUuid());
 
         if (viewer.isResourcePackApplied()) {
             return hasVoice ? node.getNode("status", "voice", "rp").getString()
