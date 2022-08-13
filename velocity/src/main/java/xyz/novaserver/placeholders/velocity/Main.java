295017e18ff2c9ac057bbb1b2be5ab0542caee1c
Lui798 <42649201+Lui798@users.noreply.github.com>
2022-08-13 04:48:13 -0500

    Remove chat filter

--- velocity/src/main/java/xyz/novaserver/placeholders/velocity/Main.java
+++ velocity/src/main/java/xyz/novaserver/placeholders/velocity/Main.java
@@ -13,7 +13,6 @@ import xyz.novaserver.placeholders.common.Plugin;
 import xyz.novaserver.placeholders.common.command.PHCommandExecutor;
 import xyz.novaserver.placeholders.common.messaging.PluginPlatform;
 import xyz.novaserver.placeholders.common.util.Config;
-import xyz.novaserver.placeholders.velocity.chat.ChatFilterListener;
 import xyz.novaserver.placeholders.velocity.command.VelocityCommand;
 import xyz.novaserver.placeholders.velocity.listener.VelocityClientListener;
 import xyz.novaserver.placeholders.velocity.listener.VelocityProxyConnection;
@@ -27,7 +26,6 @@ public class Main implements Plugin {
     private final Path dataDirectory;
 
     private Placeholders placeholders;
-    private ChatFilterListener chatFilter;
 
     @Inject
     public Main(ProxyServer server, Logger logger, @DataDirectory Path dataDirectory) {
@@ -40,7 +38,6 @@ public class Main implements Plugin {
     public void onProxyInitialization(ProxyInitializeEvent event) {
         placeholders = new Placeholders(this, new TABExpansion(),
                 new Config(this, new File(dataDirectory.toFile(), "config.yml"), "velocity-config.yml"));
-        chatFilter = new ChatFilterListener(this);
 
         if (placeholders.isUsingProxyData()) {
             placeholders.setProxyConnection(new VelocityProxyConnection(this));
@@ -88,7 +85,5 @@ public class Main implements Plugin {
     }
 
     @Override
-    public void reload() {
-        chatFilter.reload();
-    }
+    public void reload() {}
 }
